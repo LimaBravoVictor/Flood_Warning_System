5ae0a61bd8bb2a16852084a5bf6ae3ce3858e9ac
LimaBravoVictor <lbv21@cam.ac.uk>
2022-01-27 13:21:11 +0000

    Started Task

---
+++ Task1B.py
@@ -0,0 +1,7 @@
+#Luke Vogt
+
+import floodsystem.station as station
+import floodsystem.geo as geo
+if __name__ == "__main__":
+    print("*** Task 1B: CUED Part IA Flood Warning System ***")
+    run()
