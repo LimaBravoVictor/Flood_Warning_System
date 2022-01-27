53112fce5061303fc3df0c87569536c750e139da
LimaBravoVictor <lbv21@cam.ac.uk>
2022-01-27 13:31:32 +0000

    Worked on task, geo.stations_by_distance is not working, moving on

--- Task1B.py
+++ Task1B.py
@@ -1,7 +1,20 @@
 #Luke Vogt
 
 import floodsystem.station as station
+import floodsystem.stationdata as data
 import floodsystem.geo as geo
+
+centre = (52.2053, 0.1218)
+
+def run():
+    station_list = data.build_station_list()
+    #Find distances
+    distance_list = geo.stations_by_distance(station_list, centre)
+    closest = distance_list[:10]
+    futhest = distance_list[-10:]
+    print(closest)
+
+
 if __name__ == "__main__":
     print("*** Task 1B: CUED Part IA Flood Warning System ***")
     run()
