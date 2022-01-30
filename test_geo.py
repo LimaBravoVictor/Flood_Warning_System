2975adc4709e4aa42d98f1077248318af180dc8c
LimaBravoVictor <lbv21@cam.ac.uk>
2022-01-30 15:14:37 +0000

    Built test_rivers_by_station_number()

--- test_geo.py
+++ test_geo.py
@@ -1,5 +1,6 @@
 """Unit test for the geo module"""
 
+import string
 import floodsystem.geo as geo
 import floodsystem.station as station
 
@@ -23,7 +24,7 @@ def test_stations_by_distance():
     #Check Types
     assert type(output) == list
     assert type(output[0])== tuple
-    #assert isinstance (station.MonitoringStation(), output[0][0])
+    assert isinstance (station.MonitoringStation(), output[0][0])
     #Check Values
     assert output[0][0].name == "Station2"
     assert output[1][0].name == "Station3"
@@ -31,3 +32,41 @@ def test_stations_by_distance():
     assert round(output[0][1]-d2, 3)==0
     assert round(output[1][1]-d3, 3)==0
     assert round(output[2][1]-d1, 3)==0
+
+def test_rivers_by_station_number():
+    #create list of test rivers:
+    test_rivers =[]
+    for i in range (1, 101):
+        test_rivers.append(("River{}".format(i), i))
+    #Create empty stationlist:
+    station_list=[]
+    #Create stations on those rivers
+    for i in test_rivers:
+        for j in range( i[1]):
+            sid = "sid{},{}".format(i[0], j)
+            mid = "md{},{}".format(i[0], j)
+            name = "name{},{}".format(i[0], j)
+            coord = (j, -j)
+            trange = (-j,j)
+            town ="town{},{}".format(i[0], j)
+            station_list.append(station.MonitoringStation(sid,mid, name, coord, trange, i[0], town))
+    
+    #Test Lengths
+    for i in range(1, 100):
+        output = geo.rivers_by_station_number(station_list, i)        
+        assert len(output) == i
+    #Test type returns
+    output = geo.rivers_by_station_number(station_list, 10)
+    assert type(output)== list
+    assert type(output[0])== tuple
+    assert type(output[0][0])== string
+    assert type(output[0][1])== int
+    #Value checks
+    assert output[0][0] == "River100"
+    assert output[9][0] == "River90"
+    assert output[0][1] == 100
+    assert output[9][1] == 90
+    for i in range (1,output.len()+1):
+        pass
+
+test_rivers_by_station_number()
