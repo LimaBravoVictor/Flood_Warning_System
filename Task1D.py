2c47f6fcb8f33f199eae4dd8b8f922475bfc1f63
LimaBravoVictor <lbv21@cam.ac.uk>
2022-01-30 14:19:38 +0000

    Implimented, not sure about the number of stations

--- Task1D.py
+++ Task1D.py
@@ -7,18 +7,18 @@ import floodsystem.geo as geo
 rivers_of_intrest = ["River Aire","River Cam","River Thames",]
 
 def run():
-    print("Number of rivers with at lease one monitoring station:")
+    print("Number of rivers with at least one monitoring station:")
+    # At least one == one ore more
     station_list = data.build_station_list()
-    rivers = geo.rivers_with_station(station_list)
-    print (len(rivers,))
+    rivers = sorted(geo.rivers_with_station(station_list))
+    print ("{} stations. - First 10 - {}".format(len(rivers), rivers[:10]))
+
     by_river = geo.stations_by_river(station_list)
     for river in rivers_of_intrest:
         print ("\nStations on the river "+river)
         #Get names
         names = [r.name for r in by_river[river]]
         print (sorted(names))
-
-
 
 
 if __name__ == "__main__":
