b1d6d67cee4330b920b5faf3ccf018f3c121665b
Songha Kim <sk2161@cam.ac.uk>
2022-02-03 17:27:42 +0000

    modified

--- Task1D.py
+++ Task1D.py
@@ -1,24 +1,25 @@
-#Luke Vogt
+# Luke Vogt
 
 import floodsystem.station as station
 import floodsystem.stationdata as data
 import floodsystem.geo as geo
 
-rivers_of_intrest = ["River Aire","River Cam","River Thames",]
+rivers_of_intrest = ["River Aire", "River Cam", "River Thames", ]
+
 
 def run():
     print("Number of rivers with at least one monitoring station:")
     # At least one == one ore more
     station_list = data.build_station_list()
     rivers = sorted(geo.rivers_with_station(station_list))
-    print ("{} stations. - First 10 - {}".format(len(rivers), rivers[:10]))
+    print("{} stations. - First 10 - {}".format(len(rivers), rivers[:10]))
 
     by_river = geo.stations_by_river(station_list)
     for river in rivers_of_intrest:
-        print ("\nStations on the river "+river)
-        #Get names
+        print("\nStations on the river " + river)
+        # Get names
         names = [r.name for r in by_river[river]]
-        print (sorted(names))
+        print(sorted(names))
 
 
 if __name__ == "__main__":
