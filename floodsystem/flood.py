9fa36b3b779275fb7fe5b99f0aee6da071da640c
Songha Kim <sk2161@cam.ac.uk>
2022-02-21 10:21:45 +0000

    Add function for task 2C

--- floodsystem/flood.py
+++ floodsystem/flood.py
@@ -20,3 +20,24 @@ def stations_level_over_threshold(stations, tol):
     station_list.sort(key=lambda item: item[1], reverse=True)
 
     return station_list
+
+
+def stations_highest_rel_level(stations, N):
+    """
+    Returns a list of the N stations (objects) at which the water level, 
+    relative to the typical range, is highest.
+    Sorted in descending order by relative level
+    """
+
+    update_water_levels(stations)
+
+    station_list = []
+    for station in stations:
+        relative_level = station.relative_water_level()
+        if relative_level:
+            station_list.append((station))
+
+    highest_water_level = station_list[:N]
+    highest_water_level.sort(key=lambda item: item.relative_water_level(), reverse=True)
+
+    return highest_water_level
