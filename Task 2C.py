c5cacfea22856ef7967135054f92ae042ea6be4f
Songha Kim <sk2161@cam.ac.uk>
2022-02-21 10:21:26 +0000

    Created

---
+++ Task 2C.py
@@ -0,0 +1,21 @@
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_highest_rel_level
+
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    # highest relative water level stations
+    highest_water_level = stations_highest_rel_level(stations, 10)
+
+    for i in highest_water_level:
+        print(i.name, i.relative_water_level())
+
+
+if __name__ == "__main__":
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
+    run()
