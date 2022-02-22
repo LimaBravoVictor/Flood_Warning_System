44fbca09ec5bcb7b38b4de371709f6cde9b4a5d8
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-22 22:43:23 +0000

    Begun

---
+++ floodsystem/analysis.py
@@ -0,0 +1,9 @@
+"""Module for analysing station data"""
+
+from matplotlib import dates
+import numpy as np
+import datetime
+from .station import MonitoringStation
+
+def polyfit(dates, levels, p):
+    """From a list of dates and levels returns a best fit numpy polynomial of order p"""
