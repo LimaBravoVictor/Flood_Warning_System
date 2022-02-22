166e0aea1a39c31ee6a7acc58b2db8b28f8b8962
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-22 22:29:24 +0000

    Started

--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -8,4 +8,6 @@ import matplotlib as plt
 def plot_water_levels(station, dates, levels):
     """Station is a monitoring station object
     Dates (tuple of datetime tupes(Year, mounth, day)) define beteen which dates
-    """
+    levels (tuple of two floats) defines the limits of the graph"""
+
+    mid = station.measure_id
