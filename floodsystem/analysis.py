25d2b91c42a0a59fc5a6aa08993bff580628cbf6
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-22 23:31:55 +0000

    Added slice

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -6,4 +6,5 @@ import datetime
 from .station import MonitoringStation
 
 def polyfit(dates, levels, p):
-    """From a list of dates and levels returns a best fit numpy polynomial of order p"""
+    """From a list of dates and levels returns a best fit numpy polynomial of order p, and the shift of the date axis"""
+    lastdate= dates[:-1]
