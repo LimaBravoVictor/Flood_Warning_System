fd0dd0089e41ac53c6fb075555a0de9e41e674e6
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-23 00:28:52 +0000

    Fixed bugs intoduced in previous commit

--- floodsystem/station.py
+++ floodsystem/station.py
@@ -49,7 +49,7 @@ class MonitoringStation:
 
     def relative_water_level(self):
 
-        if type(self.latest_level)==float and self.typical_range_consistent():
+        if (type(self.latest_level)==float or type(self.latest_level)==int) and self.typical_range_consistent():
             relative_level = (self.latest_level / (self.typical_range[1] - self.typical_range[0]))-1
             return relative_level
         else:
