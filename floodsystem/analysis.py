c2d864e895e9629d5007493ab55f95b91aa9b914
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-24 23:22:48 +0000

    Done first pass, maybe needs sorting function

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -6,5 +6,17 @@ import datetime
 from .station import MonitoringStation
 
 def polyfit(dates, levels, p):
-    """From a list of dates and levels returns a best fit numpy polynomial of order p, and the shift of the date axis"""
-    lastdate= dates[:-1]
+    """From a list of dates and levels returns a best fit numpy polynomial of order p, 
+    and the shift of the date axis as a datetime object"""
+    if dates[0] < dates [-1]:
+        lowest_date = dates[0]
+    else:
+        lowest_date = dates[-1]
+    #Use datetime function to deduct the lowest date:
+    delta_dates =[]
+    for d in dates:
+        delta_dates.append(d - lowest_date)
+    #Then convert to floats, reducing floating point errors:
+    float_dates = dates.date2num(delta_dates)
+    polyline = np.polyfit(np.array(float_dates),np.array(levels), p)
+    return polyline, lowest_date
