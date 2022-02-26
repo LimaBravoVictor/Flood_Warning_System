5974b0be421431d8fb10d68ddb51f570dec2b34a
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-26 00:43:13 +0000

    Finished task 2F, code is fragile

--- floodsystem/analysis.py
+++ floodsystem/analysis.py
@@ -1,22 +1,25 @@
 """Module for analysing station data"""
 
-from matplotlib import dates
+from ast import Import
+
+
+import matplotlib.dates as dt
 import numpy as np
 import datetime
-from .station import MonitoringStation
+from floodsystem.station import MonitoringStation
 
 def polyfit(dates, levels, p):
     """From a list of dates and levels returns a best fit numpy polynomial of order p, 
     and the shift of the date axis as a datetime object"""
-    if dates[0] < dates [-1]:
-        lowest_date = dates[0]
-    else:
-        lowest_date = dates[-1]
+    lowest_date = dates[-1]
     #Use datetime function to deduct the lowest date:
     delta_dates =[]
     for d in dates:
         delta_dates.append(d - lowest_date)
     #Then convert to floats, reducing floating point errors:
-    float_dates = dates.date2num(delta_dates)
+    float_dates =[]
+    for i in delta_dates:
+        float_dates.append((dt.date2num(i/datetime.timedelta(microseconds=1))))
+        #dividing by days =1 converts timedelta back into datetime
     polyline = np.polyfit(np.array(float_dates),np.array(levels), p)
     return polyline, lowest_date
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,9 +1,14 @@
 
 """Submodule used for creating plots of monitoring station data:"""
 
+from pyrsistent import b
 from floodsystem.station import  MonitoringStation
 import matplotlib.pyplot as plt
+import matplotlib.dates as dt
 from floodsystem.analysis import polyfit
+import numpy as np
+import datetime
+
 
 def lables_and_title(title):
     """Adds date to x axis, Water level to the yaxis, rotates x tages 45 degrees and addes a title"""
@@ -18,22 +23,20 @@ def plot_high_low(dates, low, high):
     list = []
     for i in range (0, len(dates)):
         list.append(low)
-    plt.plot(dates, list)
+    plt.plot(dates, list, '--', color = 'g',)
     list = []
     for i in range (0, len(dates)):
         list.append(high)
-    plt.plot(dates, list)
+    plt.plot(dates, list, '--', color = 'g')
 
 
 def plot_water_levels(station, dates, levels):
     """Plots water levlels at a Station, where station is a monitoring station object;
-    dates are a list of dates
+    dates are a list of sorted dates
     levels are a corresponding list of levels"""
-
-    plt.plot(dates, levels)
-    #Plot low and high
     if station.typical_range_consistent:
         plot_high_low(dates, station.typical_range[0], station.typical_range[1])
+    plt.plot(dates, levels, color='b')
     lables_and_title(station.name)
     plt.tight_layout()
     plt.show()
@@ -42,17 +45,25 @@ def plot_water_levels(station, dates, levels):
 
 def plot_water_level_with_fit(station, dates, levels, p, high_low =False):
     """Plots water levlels at a Station, where station is a monitoring station object;
-    dates are a list of dates
+    dates are a list of sorted dates
     levels are a corresponding list of levels
     Also ploys the polynomial line of best fit to degree p"""
-
-    plt.plot(dates, levels)
-
-    polyline = polyfit(dates, levels, p)
-
-    if station.typical_range_consistent and high_low:
+    plt.clf()
+    if station.typical_range_consistent and True:
         plot_high_low(dates, station.typical_range[0], station.typical_range[1])
 
+    plt.plot(dates, levels, 'b')
+
+    polyline, start = polyfit(dates, levels, p)
+    poly = np.poly1d(polyline)
+    num_dates = []
+    for i in dates:
+        num_dates.append(dt.date2num((i -start)/datetime.timedelta(microseconds=1)))
+    #Evalute
+    polylevels = []
+    for i in num_dates:
+        polylevels.append(poly(i))
+    plt.plot(dates, polylevels, 'r')
     # Lables
     lables_and_title(station.name)
     plt.tight_layout()
