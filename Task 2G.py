7dac4fae16c70f07c6ea3997ddc370369de18c56
Songha Kim <sk2161@cam.ac.uk>
2022-02-27 20:14:30 +0000

    modified

--- Task 2G.py
+++ Task 2G.py
@@ -6,27 +6,35 @@ and rate the risk at severe/high/moderate/low."""
 from floodsystem.stationdata import build_station_list
 from floodsystem.flood import flood_risk_rate
 from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.flood import stations_level_over_threshold
+from floodsystem.analysis import gradient
 import datetime
-import floodsystem.plot as plot
 
 
 def run():
     stations = build_station_list()
 
-    # risk coefficient for each station (give list of tuples (station, risk coeff))
-    station_risk_coeff = flood_risk_rate(stations)
+    # severe : relative water level > 2 and gradient > 0.3
+    # high : relative water level > 1 and gradient > 0.1
+    # moderate : relative water level < 1 and gradient > 0
+    # low : else
+    severe_station = stations_level_over_threshold(stations, 2)
+    severe_town = []
 
-    # select the highest risk coeff for the town that has multiple stations.
-    towns ={}
-    for st in station_risk_coeff:
-        if st[0].town in towns:
-            if st[1]
+    for st in severe_station:
+        dates, levels = fetch_measure_levels(st[0].measure_id, datetime.timedelta(1))
+        # water level increases more than 0.3m/day
+        try:
+            gradient(dates, levels)
+        except ValueError:
 
-            elif st[1] > towns[st[0].town]:
-                towns[st[0].town] = st[1]
-        else:
-            towns[st[0].town] = st[1]
-    print (towns)
+        if gradient(dates, levels) >= 0.3:
+            severe_town.append(st.town)
+        return severe_town
+
+    print("{}: severe".format(severe_town))
+
+
 if __name__ == "__main__":
     print("*** Task 2G: CUED Part IA Flood Warning System ***")
     run()
