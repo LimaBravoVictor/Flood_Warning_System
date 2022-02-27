df424cbeb30480be722107c6533f07b2ea0179f1
LimaBravoVictor <lbv21@cam.ac.uk>
2022-02-27 19:19:39 +0000

    Ditto

--- floodsystem/datafetcher.py
+++ floodsystem/datafetcher.py
@@ -132,9 +132,9 @@ def fetch_measure_levels(measure_id, dt):
     for measure in data['items']:
         # Convert date-time string to a datetime object
         d = dateutil.parser.parse(measure['dateTime'])
-
-        # Append data
-        dates.append(d)
-        levels.append(measure['value'])
+        if 'value' in measure:
+            # Append data
+            dates.append(d)
+            levels.append(measure['value'])
 
     return dates, levels
