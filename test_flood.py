d437c7f7fc40a949dc753bdd9eecb6b6a66cac88
LimaBravoVictor <lbv21@cam.ac.uk>
2022-03-01 12:59:57 +0000

    Added test_station_over_threshold

--- test_flood.py
+++ test_flood.py
@@ -2,8 +2,17 @@
 
 from tkinter.tix import StdButtonBox
 from sklearn.preprocessing import StandardScaler
-import floodsystem.flood
+import floodsystem.flood as flood
 from floodsystem.station import MonitoringStation
 
-def test_stations_over_threshold():
-    StA = MonitoringStation("01","2","Bob",(1,1), (0, 1), "Humber", "town")
+def test_stations_level_over_threshold():
+    sta = MonitoringStation("01","2","Bob",(1,1), (0, 1), "Humber", "town")
+    sta.latest_level = 0
+    stb = sta
+    stb.latest_level =1
+    stc =sta
+    stc.latest_level =2
+    list = (sta, stb, stc)
+    st_list = flood.stations_level_over_threshold(list, 1)
+    assert (stc, 1) in st_list
+    
