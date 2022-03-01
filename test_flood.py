7856266df8917c5bd871afa95d92560509082593
LimaBravoVictor <lbv21@cam.ac.uk>
2022-03-01 12:54:32 +0000

    Started

--- test_flood.py
+++ test_flood.py
@@ -1,5 +1,9 @@
 """Unit test for the flood module"""
 
+from tkinter.tix import StdButtonBox
+from sklearn.preprocessing import StandardScaler
 import floodsystem.flood
+from floodsystem.station import MonitoringStation
 
 def test_stations_over_threshold():
+    StA = MonitoringStation("01","2","Bob",(1,1), (0, 1), "Humber", "town")
